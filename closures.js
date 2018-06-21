0122ed549bd881f7588f67f47547b3a54a8e8426
Goyo <gregory.vargas18@gmail.com>
2018-06-21 15:43:21 -0500

    completed closures

--- closures.js
+++ closures.js
@@ -203,9 +203,15 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+
+    function timer(index) {
+      setTimeout(function() {
+        console.log(index);
+      }, index * 1000);
+    }
+
+    timer(i); 
   }
 }
 timeOutCounter();
+console.log(timeOutCounter());
